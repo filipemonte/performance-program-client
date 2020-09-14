049771525fda1db0beca939525012b28e714a972
Filipe Albuquerque <filipe.albuquerque@serpro.gov.br>
2020-09-14 15:05:36 -0300

    api heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const myConfig = { apiUrl: 'http://localhost:3001/api' };
+export const myConfig = { apiUrl: 'https://performanceprogram-api.herokuapp.com/api/' };
 
 
 export const firebaseConfig = {
